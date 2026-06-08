c94e89fe45cbb805845b576e614b4aef7a1b1448
Montse <121003379@alumno.enp.unam.mx>
2026-06-08 15:18:08 -0600

    feat(recursos): agrega pantalla de recursos

--- Dynamics/php/recursos.php
+++ Dynamics/php/recursos.php
@@ -5,25 +5,61 @@ include 'layout.php';
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewpport" content ="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../Statics/styles/style.css">
+    <meta name="viewport" content ="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../Statics/styles/recursos.css">
 
 </head>
 <body>
-    <main>
+    <main class="recursos">
         <h2>Recursos</h2>
-        <section class="contenedor-modulos">
-            <article class="cuadro">
-                <h3>
-                <a href="recursos.php">Recursos</a>
-                </h3>
-            </article>
-            <article class="cuadro">
-                <h3>Actividades/tareas</h3>
-            </article>
-            <article class="cuadro">
-                <h3>Asistencia</h3>
-            </article>
+
+        <section class="archivos">
+            <h3>Archivos</h3>
+            <div id="cuadro-arch">
+                <article class="cuadro-re">
+                    <h3>Archivo 1</h3>
+                </article>
+                <article class="cuadro-re">
+                    <h3>Archivo 2</h3>
+                </article>
+            </div>
+        </section>
+
+        <section id="ejercicios">
+            <h3>Ejercicios</h3>
+            <div id="cuadro-ejer">
+                <article class="cuadro-re">
+                    <h3>Ejercicio 1</h3>
+                </article>
+                <article class="cuadro-re">
+                    <h3>Ejercicio 2</h3>
+                </article>
+            </div>
+        </section>
+
+        <section id="enlaces">
+            <h3>Enlaces</h3>
+            <div id="cuadro-enl">
+                <article class="cuadro-re">
+                    <h3>Enlace 1</h3>
+                </article>
+                <article class="cuadro-re">
+                    <h3>Enlace 2</h3>
+                </article>
+            </div>
+        </section>
+
+        <section id="scripts">
+            <h3>Scripts</h3>
+            <div id="cuadro-script">
+                <article class="cuadro-re">
+                    <h3>Script 1</h3>
+                </article>
+                <article class="cuadro-re">
+                    <h3>Script 2</h3>
+                </article>
+            </div>
         </section>
     </main>
 </body>
+</html>
