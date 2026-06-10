f952c544a892aac268ce09023c055f8b0605206a
Montse <121003379@alumno.enp.unam.mx>
2026-06-10 13:31:30 -0600

    feat(perfil): agrega vista del perfil

---
+++ Dynamics/php/perfil.php
@@ -0,0 +1,34 @@
+<?php
+include 'layout.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewpport" content ="width?device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../Statics\styles\perfil.css">
+    <title>Página Principal </title>
+</head>
+<body>
+    <main>
+        <div class="cuadro_alumno">
+            <h2>nombre del alumno</h2>
+        </div>
+        <div>
+            <h3>Correo:</h3>
+            <h3>Nombre:</h3>
+            <h3>Numero de cuenta /ID profe:</h3>
+            <h3>Grupo(s):</h3>
+        </div>
+        <div class="cuestionario">
+            <h2>Cuestionario</h2>
+        </div>
+        <div>
+            <h3>Estilo de aprendizaje</h3>
+            <h3>Habitos de estudio X/X</h3>
+            <h3>Tiempo disponible X/X</h3>
+            <h3>Equipo de computo X/X</h3>
+        </div>
+    </main>
+</body>
+</html>
