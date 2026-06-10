23b8bf72804e0d4c1e77aeb50bbe6395239d72d8
Ma. Inés Solís Orantes <325089878@alumno.enp.unam.mx>
2026-06-09 20:45:18 -0600

    feat: Maquetado y CSS principal de la página de inicio

---
+++ Dynamics/php/inicio.php
@@ -0,0 +1,24 @@
+<?php 
+    include 'layout.php'
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewpport" content ="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Statics/styles/layout.css">
+
+</head>
+<body>
+    <main class="contenido">    
+        <div id="Bienv">
+            <h2>Bienvenid@ </h2>
+        </div>
+
+        <div id="mis_grupos">
+            <h3>Mis grupos: </h3>
+            <p><a href= "modulos.php">Grupo 1</a></p>
+        </div>
+    </main>    
+</body>
+</html>
