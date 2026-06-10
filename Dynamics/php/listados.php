d871629046f1a2f10017e556e98bff872c1f1dd5
Montse <121003379@alumno.enp.unam.mx>
2026-06-09 21:10:34 -0600

    feat(listado): agrega la calificacion al listado

--- Dynamics/php/listados.php
+++ Dynamics/php/listados.php
@@ -5,9 +5,11 @@ include 'layout.php';
 $lista_alumnos = array();
 $id_grupo = '61B';
 
-    $sql = "SELECT usuarios.nombre, usuarios.apellido_p, usuarios.apellido_m
-            FROM alumnos INNER JOIN usuarios ON alumnos.id_usuario = usuarios.id_usuario
-            WHERE alumnos.id_grupo1 = '$id_grupo'";
+    $sql = "SELECT usuarios.nombre, usuarios.apellido_p, usuarios.apellido_m,
+            AVG(calificaciones.calificacion) FROM alumnos INNER JOIN usuarios 
+            ON alumnos.id_usuario = usuarios.id_usuario LEFT JOIN calificaciones
+            ON alumnos.id_alumno = calificaciones.id_alumno WHERE alumnos.id_grupo1 = '$id_grupo'
+            GROUP BY alumnos.id_alumno";
 
     $resultado_query = mysqli_query($conexion, $sql);
 
@@ -34,9 +36,15 @@ $id_grupo = '61B';
         foreach ($lista_alumnos as $alumno) {
             echo "<div class='tarjeta'>";
                 echo "<h3 class='h3_listado'>";
-                echo $alumno['nombre'] . " ";
-                echo $alumno['apellido_p'] . " ";
+                echo "Nombre:";
+                echo $alumno['nombre'];
+                echo "<br>";
+                echo "Apellido(s):";
+                echo $alumno['apellido_p'];
                 echo $alumno['apellido_m'];
+                echo "<br>";
+                echo "Promedio:";
+                echo $alumno['AVG(calificaciones.calificacion)'];
                 echo "</h3>";
             echo "</div>";
         }
