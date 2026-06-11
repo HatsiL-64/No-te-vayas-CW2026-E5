f947e06389b96436e49cdf828d89d0e00abd3912
Hatsi Lopez <324082625@alumno.enp.unam.mx>
2026-06-10 19:43:53 -0600

    fix: feat añade un link a inicio en layout y comprueba sesion en inicio

--- Dynamics/php/layout.php
+++ Dynamics/php/layout.php
@@ -1,33 +1,36 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
 
     <meta charset="UTF-8">
-    <meta name="viewpport" content ="width?device-width, initial-scale=1.0">
+    <meta name="viewpport" content="width?device-width, initial-scale=1.0">
     <meta name="author" content="Maria Solís">
     <meta name="description" content="PP">
     <link rel="stylesheet" href="../../Statics/styles/layout.css">
     <title>Página Principal </title>
 
 </head>
+
 <body>
     <nav class="navbar">
         <div class="nav-left">
-            <table id = "logos">
-            <td><img class = "logo" src = "../../Statics/media/img/logo_ete.jpg" alt = "Logo de los Estudios Tecnicos Especializados"></td>
-            <td><img class = "logo" src = "../../Statics/media/img/logo_unam.png" alt = "Logo de la UNAM"></td>
+            <table id="logos">
+                <td><img class="logo" src="../../Statics/media/img/logo_ete.jpg" alt="Logo de los Estudios Tecnicos Especializados"></td>
+                <td><img class="logo" src="../../Statics/media/img/logo_unam.png" alt="Logo de la UNAM"></td>
             </table>
             <h1>No te vayas, QuédETE</h1>
-            <img class = "foto_perfil" id="perfil_encabezado" src = "../../Statics/media/img/foto_default.jpg" alt = "Foto de perfil">
+            <img class="foto_perfil" id="perfil_encabezado" src="../../Statics/media/img/foto_default.jpg" alt="Foto de perfil">
         </div>
     </nav>
     <nav>
         <div class="menu_lateral">
-            <p><a href="#">Inicio</a></p>
+            <p><a href="./inicio.php">Inicio</a></p>
             <p><a href="#">Mi Perfil</a></p>
             <p><a href="#">Mensajes</a></p>
             <p><a href="./cerrar_sesion.php">Cerrar Sesión</a></p>
         </div>
     </nav>
 </body>
+
 </html>
