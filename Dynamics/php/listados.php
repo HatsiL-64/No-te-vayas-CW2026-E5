ce24b02952cb4ee5da91b29c835750a40fc477f3
Montse <121003379@alumno.enp.unam.mx>
2026-06-12 13:46:00 -0600

    fix(listados): corrige carga de datos

--- Dynamics/php/listados.php
+++ Dynamics/php/listados.php
@@ -4,10 +4,9 @@ include 'layout.php';
 
 $lista_alumnos = array();
 $id_grupo = '61B';
-$id = $alumno['id_alumno'];
 
     $sql = "SELECT alumnos.id_alumno, usuarios.nombre, usuarios.apellido_p, usuarios.apellido_m,
-            AVG(calificaciones.calificacion)FROM alumnos INNER JOIN usuarios
+            AVG(calificaciones.calificacion) FROM alumnos INNER JOIN usuarios
             ON alumnos.id_usuario = usuarios.id_usuario LEFT JOIN calificaciones
             ON alumnos.id_alumno = calificaciones.id_alumno WHERE alumnos.id_grupo1 = '$id_grupo'
             GROUP BY alumnos.id_alumno";
@@ -36,7 +35,7 @@ $id = $alumno['id_alumno'];
             foreach ($lista_alumnos as $alumno) {
             echo "<div class='tarjeta'>";
                 echo "<h3 class='h3_listado'>";
-                echo "<a href='perfil.php?id_alumno=$id'>";
+                echo "<a href='perfil.php?id_alumno=" . $alumno['id_alumno'] . "'>";
                     echo $alumno['nombre'] . " ";
                     echo $alumno['apellido_p'] . " ";
                     echo $alumno['apellido_m'];
