e37579febb36725a033eca290a6647442e6e39e3
Ma. Inés Solís Orantes <325089878@alumno.enp.unam.mx>
2026-06-12 12:19:18 -0600

    fix: adapta listado de alumnos a nuevo modelo de base de datos

--- Dynamics/php/listados.php
+++ Dynamics/php/listados.php
@@ -6,12 +6,12 @@ $lista_alumnos = array();
 $id_grupo = '61B';
 
     $sql = "SELECT usuarios.nombre, usuarios.apellido_p, usuarios.apellido_m,
-            AVG(calificaciones.calificacion) FROM alumnos INNER JOIN usuarios 
-            ON alumnos.id_usuario = usuarios.id_usuario LEFT JOIN calificaciones
-            ON alumnos.id_alumno = calificaciones.id_alumno WHERE alumnos.id_grupo1 = '$id_grupo'
+            AVG(calificaciones_$id_grupo.calificacion) AS promedio FROM alumnos INNER JOIN usuarios 
+            ON alumnos.id_usuario = usuarios.id_usuario LEFT JOIN calificaciones_$id_grupo
+            ON alumnos.id_alumno = calificaciones_$id_grupo.id_alumno WHERE alumnos.id_grupo1 = '$id_grupo'
             GROUP BY alumnos.id_alumno";
 
-    $resultado_query = mysqli_query($conexion, $sql);
+    $resultado_query = mysqli_query($conexion, $sql) or die("Error en el query: " . mysqli_error($conexion));
 
     if ($resultado_query){
         while ($fila = mysqli_fetch_assoc($resultado_query)) {
@@ -25,7 +25,7 @@ $id_grupo = '61B';
 <head>
     <meta charset="UTF-8">
     <meta name="viewpport" content ="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/No-te-vayas-CW2026-E5/Statics/styles/style.css">
+    <link rel="stylesheet" href="../../Statics/styles/style.css">
 </head>
 <body>
 
@@ -44,7 +44,7 @@ $id_grupo = '61B';
                 echo $alumno['apellido_m'];
                 echo "<br>";
                 echo "Promedio:";
-                echo $alumno['AVG(calificaciones.calificacion)'];
+                echo $alumno['promedio'];
                 echo "</h3>";
             echo "</div>";
         }
