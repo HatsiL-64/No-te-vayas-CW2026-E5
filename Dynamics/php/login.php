4e31822f3905cfcb06dc0a701e58c840393e067a
Hatsi Lopez <324082625@alumno.enp.unam.mx>
2026-06-12 15:44:08 -0600

    feat: Pasa las contraseñas por una funcion hash

--- Dynamics/php/login.php
+++ Dynamics/php/login.php
@@ -1,11 +1,13 @@
 <?php
 session_start();
+require 'config.php';
+require 'validaciones.php';
+
 $error = null;
 
 if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST["usuario"])) {
-  require 'config.php';
-  $usuario_entrada = trim($_POST["usuario"]);
-  $password = trim($_POST["password"]);
+  $usuario_entrada = sanitizar_entrada($conexion, $_POST["usuario"]);
+  $password_entrada = sanitizar_entrada($conexion, $_POST["password"]);
   $tipo_usuario = "";
 
   if (isset($_POST["tipo_usuario"])) {
@@ -17,9 +19,11 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST["usuario"])) {
   } else if ($tipo_usuario == "profesor") {
     $usuario = "2" . $usuario_entrada;
   } else {
+    $tipo_usuario = 3;
     $usuario = "3" . $usuario_entrada;
   }
 
+  $password = hash("sha256", $password_entrada);
   $sql = "SELECT nombre, id_usuario, tipo_usuario FROM usuarios WHERE id_usuario = '$usuario' AND password = '$password'";
   $resultado = mysqli_query($conexion, $sql);
   $registro = mysqli_fetch_assoc($resultado);
